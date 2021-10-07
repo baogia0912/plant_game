a0a3c1f9797aae9947b8c5b09162cd5120c2435b
baogia0912 <baogia0912@gmail.com>
2021-10-07 14:38:29 +0700

    help

--- window.py
+++ window.py
@@ -1,4 +1,12 @@
-import pygame 
+import pygame
 # make a window appear with the title name plant game
-
-
+backgound_colour = (255, 255, 255)
+screen = pygame.display.set_mode((300, 300))
+pygame.display.set_caption('plant game')
+screen.fill(102, 51, 0)
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False

db5510b119ee0e0fe2d64bb5e0d1b2e9abc16548
baogia0912 <51979397+baogia0912@users.noreply.github.com>
2021-10-07 15:53:40 +0700

    code refactored

--- window.py
+++ window.py
@@ -1,22 +1,27 @@
 import pygame, sys
 from pygame.locals import *
-#make the window bigger with fullscreen function
-backgound_colour = (255, 255, 255)
-screen = pygame.display.set_mode((1000, 700), pygame.RESIZABLE)
-pygame.display.set_caption('plant game')
-screen.fill((102, 51, 0))
-pygame.display.flip()
-running = True
-while running:
+#make 2 boxes 1 big 1small big cant move small move with mouse
 
-    for event in pygame.event.get():
-        if event.type == VIDEORESIZE:
-            screen.fill((102, 51, 0))
+backgound_colour = (102, 51, 0) #brown
 
-        if event.type == pygame.QUIT:
-            running = False
-        
-    pygame.display.update()
+def main():
+    screen = pygame.display.set_mode((1000, 700), pygame.RESIZABLE)
+    pygame.display.set_caption('plant game')
+    screen.fill(backgound_colour)
+    launch_game(True)
 
+    
+def launch_game(running):
 
+    while running:
+        for event in pygame.event.get():
+            if event.type == VIDEORESIZE:
+                screen.fill(backgound_colour)
 
+            if event.type == pygame.QUIT:
+                running = False
+            
+        pygame.display.update()
+
+if __name__ == '__main__':
+    main()

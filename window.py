7d0aef1ed656de1c8013b0117a4f080e9c74a921
baogia0912 <51979397+baogia0912@users.noreply.github.com>
2021-10-07 14:46:51 +0700

    fixed color adding

--- window.py
+++ window.py
@@ -1,12 +1,13 @@
 import pygame
-# make a window appear with the title name plant game
+#make the window bigger with fullscreen function
 backgound_colour = (255, 255, 255)
 screen = pygame.display.set_mode((300, 300))
 pygame.display.set_caption('plant game')
-screen.fill(102, 51, 0)
+screen.fill((102, 51, 0))
 pygame.display.flip()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
